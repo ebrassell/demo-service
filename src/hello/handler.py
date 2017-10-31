4e23186886cd3138275a33702f021f71f5aaa7c3
Eric Brassell <ebrassell@indigoag.com>
2017-10-31 08:32:01 -0400

    with s3

--- src/hello/handler.py
+++ src/hello/handler.py
@@ -1,9 +1,24 @@
 import json
-
+import logging
+import urllib.parse
+LOG = logging.getLogger(__name__)
 
 def hello(event, context):
+    if 'Records' in event:
+        for record in event['Records']:
+            try:
+                LOG.info('Processing record %s', record)
+                bucket = record['s3']['bucket']['name']
+                key = urllib.parse.unquote_plus(record['s3']['object']['key'])
+                LOG.info('New s3 file recieved - %s', key)
+            except Exception as exception:  # pylint:disable=broad-except
+                LOG.error(exception)
+                LOG.error('Caught error: ', exc_info=True)
+    else:
+        LOG.warning('No records in payload')
+
     body = {
-        "message": "Hello",
+        "message": "Hello---",
         "input": event
     }
 
